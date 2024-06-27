05731ea2061cb57ec84821aabcf6ecf65213df8c
Chad Martin <chadmartincoding@gmail.com>
2024-06-26 23:05:48 -0700

    git ignore and random logic

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 require("dotenv").config();
-fetch(
-  "https://nuthatch.lastelm.software/v2/birds?page=1&pageSize=25&operator=AND",
-  {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      "API-Key": process.env["BIRD_API_KEY"],
-    },
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  let randomPage = Math.floor(Math.random() * 41);
+  // If it randomly hits 0 just grab first one
+  if (randomPage === 0) {
+    randomPage = 1;
   }
-)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Error:", error));
+
+  const response = await fetch(
+    `https://nuthatch.lastelm.software/v2/birds?page=${randomPage}&pageSize=25&operator=AND=`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "API-Key": process.env["BIRD_API_KEY"],
+      },
+    }
+  );
+
+  const { entities } = await response.json();
+  const birdsWithImages = entities.filter((bird) => bird.images.length);
+  const randomBirdIndex = Math.floor(
+    Math.random() * birdsWithImages.length + 1
+  );
+
+  return birdsWithImages[randomBirdIndex];
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
